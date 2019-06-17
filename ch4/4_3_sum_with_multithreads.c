4c17f4d0a8ce92f5a7aa6944a5ec1bd19fa7a507
Zeng Chi-Ruei <s1023030@gmail.com>
2019-06-18 00:48:08 +0800

    add the time calculation to 4_3_sum_with_multithreads,but get some bugs

--- ch4/4_3_sum_with_multithreads.c
+++ ch4/4_3_sum_with_multithreads.c
@@ -1,31 +1,48 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-int sum=0;
-int numOfThreads=5;
+#include <time.h>
+
+long long int sum=0;
 struct thread_data {
     int start;
     int end;
 };
 void *runner(void *param);
+void calSum(int numToSum,int numOfThreads);
 int main(int argc, char *argv[])
 {
-	pthread_t tid[numOfThreads]; 
-	pthread_attr_t attr[numOfThreads];
+	clock_t start,end;
+	double time=0.0;
 	int numToSum=atoi(argv[1]);
-	struct thread_data* thread_array = malloc(numOfThreads * sizeof(struct thread_data));
+	int i;
+	
 	if (argc != 2) {
 		fprintf(stderr,"usage: a.out <integer value>\n");
 		return -1;
 	}
-	if (atoi(argv[1]) < 0) {
-		fprintf(stderr,"%d must be >= 0\n",atoi(argv[1]));
+	if (numToSum < 0) {
+		fprintf(stderr,"%d must be >= 0\n",numToSum);
 		return -1;
 	}
-	if (atoi(argv[1]) < numOfThreads) {
-		fprintf(stderr,"Arg must be >= %d\n",numOfThreads);
-		return -1;
+	
+	for(i=2;i<=6;i++){
+		start = clock();
+		calSum(numToSum,i);
+		end = clock();
+		time= (double)(end - start)/1000.0;
+		printf("Using %lf second.\n",time);
+		printf("==========================\n");
 	}
+	
+	
+	return 0;
+}
+void calSum(int numToSum,int numOfThreads){
+	sum=(long long int)0;
+	pthread_t tid[numOfThreads]; 
+	pthread_attr_t attr[numOfThreads];
+	struct thread_data* thread_array = malloc(numOfThreads * sizeof(struct thread_data));
 	int i;
 	int interval=numToSum/numOfThreads;
 	for(i=0;i<numOfThreads;i++){
@@ -48,14 +65,13 @@ int main(int argc, char *argv[])
 		pthread_join(tid[i],NULL);
 	}
 	free(thread_array);
-	printf("sum = %d\n",sum);
-	return 0;
+	printf("sum = %lld .\n",sum);
 }
-void *runner(void *param)
-{
+void *runner(void *param){
 	int lower=((struct thread_data*) param) -> start, upper= ((struct thread_data*) param) -> end;
 	int i;
-	for (i = lower; i <= upper; i++)
-		sum += i;
+	for (i = lower; i <= upper; i++){
+		sum += (long long int)i;
+	}
 	pthread_exit (0);
 }
